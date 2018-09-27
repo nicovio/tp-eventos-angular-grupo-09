7db8d6810e9da06049b9a3f3fe84c80f33c07990
nicovio <nicolasviotti90@gmail.com>
2018-09-27 00:08:06 -0300

    Creando proyecto. Agrego barra de navegación que va a estar en todas las vistas.

---
+++ src/app/perfil/perfil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'perfil',
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.css']
+})
+export class PerfilComponent {
+  title = 'perfil';
+}
---
+++ src/app/perfil/perfil.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { PerfilComponent } from './perfil.component';
+
+@NgModule({
+  declarations: [PerfilComponent],
+  imports: [],
+  providers: [],
+  bootstrap: [PerfilComponent]
+})
+export class PerfilModule {}
