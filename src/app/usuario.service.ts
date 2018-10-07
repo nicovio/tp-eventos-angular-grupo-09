d28e73b2dc9370dd3fe6576ca9b406288d08455f
nicovio <nicolasviotti90@gmail.com>
2018-10-07 00:35:26 -0300

    Sacando scrollbar de la lista de amigos.

--- src/app/usuario.service.ts
+++ src/app/usuario.service.ts
@@ -53,6 +53,22 @@ export class StubUsuarioService extends UsuarioService {
         usuario.agregarAmigo(new Usuario('Carlos', 'Gardel', '@carlitosgardel'));
         usuario.agregarAmigo(new Usuario('Lady', 'Gaga', '@ladygaga'));
         usuario.agregarAmigo(new Usuario('Fernando', 'Dodino', '@dodain'));
+        usuario.agregarAmigo(new Usuario('Timothy', 'Drake', '@theRedOne'));
+        usuario.agregarAmigo(new Usuario('Catherine', 'Grant', '@catGrant'));
+        usuario.agregarAmigo(new Usuario('Perry', 'White', '@whiteDP'));
+        usuario.agregarAmigo(new Usuario('James', 'Gordon', '@jimG'));
+        usuario.agregarAmigo(new Usuario('James', 'Olsen', '@jimmy_olsn'));
+        usuario.agregarAmigo(new Usuario('Katherine', 'Kane', '@kathyKane'));
+        usuario.agregarAmigo(new Usuario('Julia', 'Pennyworth', '@JuliiPen'));
+        usuario.agregarAmigo(new Usuario('Jackson', 'Hyde', '@JHyde'));
+        usuario.agregarAmigo(new Usuario('Maravilla', 'Martinez', '@SaliDeAhiMaravilla'));
+        usuario.agregarAmigo(new Usuario('Marcelo', 'Tinelli', '@marcelotinelli'));
+        usuario.agregarAmigo(new Usuario('Carolina', 'Ardohain ', '@pampitaoficial '));
+        usuario.agregarAmigo(new Usuario('Sean', 'Penn', '@elmachopenn'));
+        usuario.agregarAmigo(new Usuario('Jennifer', 'Lawrence', '@JSLawrence'));
+        usuario.agregarAmigo(new Usuario('Carlos', 'Gardel', '@carlitosgardel'));
+        usuario.agregarAmigo(new Usuario('Lady', 'Gaga', '@ladygaga'));
+        usuario.agregarAmigo(new Usuario('Fernando', 'Dodino', '@dodain'));
         this.agregarUsuario(usuario)
     }
 }
