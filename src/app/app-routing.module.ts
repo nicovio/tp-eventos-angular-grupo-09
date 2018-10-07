3d8dfe8634165ab190bfefa0e637c0b8b094a0f9
nicovio <nicolasviotti90@gmail.com>
2018-10-07 03:16:41 -0300

    Agrego routing a los componentes de 'mis eventos'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,29 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PerfilComponent } from './perfil/perfil.component';
 import { MisEventosComponent } from './mis-eventos/mis-eventos.component';
+import { AgendaComponent } from './mis-eventos/agenda/agenda.component';
+import { OrganizadosPorMiComponent } from './mis-eventos/organizados-por-mi/organizados-por-mi.component';
+import { InvitacionesPendientesComponent } from './mis-eventos/invitaciones-pendientes/invitaciones-pendientes.component';
 
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/mis-eventos', pathMatch: 'full' },
-  // por defecto redirigimos al perfil
+  { path: '', redirectTo: 'mis-eventos/agenda', pathMatch: 'full' },
+
   { path: 'perfil', component: PerfilComponent },
-  { path: 'mis-eventos', component: MisEventosComponent }
+  {
+    path: 'mis-eventos', component: MisEventosComponent,
+    children: [
+      { path: 'agenda', component: AgendaComponent },
+      { path: 'organizados-por-mi', component: OrganizadosPorMiComponent },
+      { path: 'pendientes', component: InvitacionesPendientesComponent }
+    ]
+  }
+
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes), RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
 export const routingComponents = [PerfilComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,23 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { MDBBootstrapModule, NavbarModule } from 'angular-bootstrap-md';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AmigosComponent } from './perfil/amigos/amigos.component';
 import { PerfilComponent } from './perfil/perfil.component';
 import { UserImageComponent } from './perfil/user-image/user-image.component';
 import { NavbarComponent } from './share/navbar/navbar.component';
-import { MaterialModule } from './material.module';
+import { MaterialModule } from './angular-material-module/material.module';
 import { MisEventosComponent } from './mis-eventos/mis-eventos.component';
 import { SidebarEventosComponent } from './mis-eventos/shared/sidebar-eventos/sidebar-eventos.component';
-
+import { AgendaComponent } from './mis-eventos/agenda/agenda.component';
+import { OrganizadosPorMiComponent } from './mis-eventos/organizados-por-mi/organizados-por-mi.component';
+import { InvitacionesPendientesComponent } from './mis-eventos/invitaciones-pendientes/invitaciones-pendientes.component';
 
 
 @NgModule({
-  declarations: [AppComponent, routingComponents, PerfilComponent, NavbarComponent, UserImageComponent, AmigosComponent, MisEventosComponent, SidebarEventosComponent],
+  declarations: [AppComponent, routingComponents, PerfilComponent, NavbarComponent, UserImageComponent, AmigosComponent, MisEventosComponent, SidebarEventosComponent, AgendaComponent, OrganizadosPorMiComponent, InvitacionesPendientesComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
