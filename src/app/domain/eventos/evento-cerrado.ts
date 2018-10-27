69334ae8ffb4d5782d98f34e352ed4af0a3863fc
nicovio <nicolasviotti90@gmail.com>
2018-10-27 06:34:01 -0300

    Ajustes mínimos

--- src/app/domain/eventos/evento-cerrado.ts
+++ src/app/domain/eventos/evento-cerrado.ts
@@ -1,6 +1,4 @@
 import Evento from "src/app/domain/eventos/evento"
-import Usuario from "../usuarios/usuario";
-import { Invitacion } from "./invitacion";
 
 export default class EventoCerrado extends Evento {
   totalPersonasInvitadas: number = 0
